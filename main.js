3422afec25b5a5e3c256c3a56a601c45add57af1
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-19 13:11:27 +0100

    finish

---
+++ main.js
@@ -0,0 +1,57 @@
+// let titre = document.querySelector("h2.ml-5");
+// console.log(titre.textContent);
+
+//////////////////
+
+// titre.textContent = "exercice 1";
+// console.log(titre.textContent);
+
+/////////////////
+
+// let para= titre.nextElementSibling;
+// para.textContent = "exercice 1"
+// console.log(para.textContent);
+
+/////////////////
+
+// let section1 = document.getElementById("users");
+// console.log(section1);
+
+///////////////////
+
+// let titre = document.querySelector("h1");
+// console.log(titre.className);
+
+////////////////
+
+// let titre = document.querySelectorAll("h1");
+// titre.forEach(titre => {
+//     console.log(titre.className);
+// });
+
+//////////////////
+
+//let attribut = document.querySelector("input");
+// console.log(attribut.attributes);
+
+/////////////////
+
+// let attribut = document.querySelector("input");
+// console.log(attribut.type);
+
+///////////////
+
+// let recup = document.getElementById("inputPassword3");
+// console.log(recup);
+// recup.setAttribute(`type`, `password`);
+// console.log(recup);
+
+/////////////////
+
+// let myInput = document.querySelector("input");
+// console.log(myInput.attributes);
+
+// myInput.setAttribute(`type`, `color`);
+// console.log(myInput);
+
+
